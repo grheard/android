66cefd58eab1ab56a65e2388bcc4edc1eb388de5
growse <github@growse.com>
2019-01-11 18:50:02 +0000

    Initial espresso tests to catch emulator issues

---
+++ project/app/src/androidTest/java/org/owntracks/android/WelcomeActivityTests.java
@@ -0,0 +1,32 @@
+package org.owntracks.android;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.owntracks.android.ui.welcome.WelcomeActivity;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.GrantPermissionRule;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class WelcomeActivityTests {
+    @Rule
+    public ActivityScenarioRule<WelcomeActivity> activityScenarioRule = new ActivityScenarioRule<>(WelcomeActivity.class);
+
+    @Rule
+    public GrantPermissionRule permissionRule = GrantPermissionRule.grant(android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+    @Test
+    public void WelcomeScreenLoadsAndResponds() {
+        onView(withId(R.id.btn_next)).perform(click());
+        onView(withId(R.id.btn_next)).perform(click());
+        onView(withId(R.id.done)).perform(click());
+    }
+}
